102c362007b65c71ac481d12d7e7bf22e9ce6e0d
Djalma Silveira <djalmasilveira10@gmail.com>
2024-03-03 20:07:59 -0300

    Feat(WebApplication-Teste): Update Company model

--- WebApplication-Teste/WebApplication-Teste/Models/Company.cs
+++ WebApplication-Teste/WebApplication-Teste/Models/Company.cs
@@ -1,6 +1,14 @@
 ﻿namespace WebApplication_Teste.Models
+
 {
     public class Company
     {
+        public int Id { get; set; }
+        public string? AvatarUrl { get; set; }
+        public string? NomeFantasia { get; set; }
+        public string? RazaoSocial { get; set; }
+        public int QtdeFuncionarios { get; set; }
+        public bool Active { get; set; }
     }
+
 }
