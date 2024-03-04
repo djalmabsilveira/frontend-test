94467479984c1aa988cbf3062270ba8087e7e944
Djalma Silveira <djalmasilveira10@gmail.com>
2024-03-03 22:06:49 -0300

    Feat(WebApplication-Teste): Refactor Program.cs

--- WebApplication-Teste/WebApplication-Teste/Program.cs
+++ WebApplication-Teste/WebApplication-Teste/Program.cs
@@ -3,26 +3,15 @@ using WebApplication_Teste.Dao.DataAccess;
 using WebApplication_Teste.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
-
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddScoped<CompanyDAO>();
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,9 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
