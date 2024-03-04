26a4a7247eba2bc3de7b53fb450145d50c460791
Djalma Silveira <djalmasilveira10@gmail.com>
2024-03-03 21:47:58 -0300

    Feat(WebApplication-Teste): Update controller

--- WebApplication-Teste/WebApplication-Teste/Controllers/CompanyController.cs
+++ WebApplication-Teste/WebApplication-Teste/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication_Teste.DataAccess;
+using WebApplication_Teste.Models;
 
 namespace WebApplication_Teste.Controllers
 {
@@ -6,9 +8,58 @@ namespace WebApplication_Teste.Controllers
     [Route("api/[controller]")]
     public class CompanyController : ControllerBase
     {
-        [HttpGet(Name = "GetWeatherForecast")]
-        public void Get()
+        private readonly CompanyDAO _companyDAO;
+
+        public CompanyController(CompanyDAO companyDAO)
         {
+            _companyDAO = companyDAO;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            var companies = await _companyDAO.GetCompaniesAsync();
+            return Ok(companies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(int id)
+        {
+            var company = await _companyDAO.GetCompanyAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return company;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCompany(Company company)
+        {
+            await _companyDAO.AddCompanyAsync(company);
+            return CreatedAtAction(nameof(GetCompany), new { id = company.Id }, company);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany(int id, Company company)
+        {
+            var updated = await _companyDAO.UpdateCompanyAsync(id, company);
+            if (!updated)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            var deleted = await _companyDAO.DeleteCompanyAsync(id);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+            return NoContent();
         }
     }
 }
